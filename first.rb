54450cf8ae71841bbdb471843f0dd095d6778b48
Cristiane Silva <cristiane.hdo@gmail.com>
2018-01-19 17:53:26 +0000

    first rspec test done

---
+++ first.rb
@@ -0,0 +1,11 @@
+def hash_to_array(hash)
+  hash.map{ |key_value| key_value }
+end
+
+
+def array_to_hash(arr)
+
+end
+
+p hash_to_array({ "a"=> "abc", "b"=> "bcd", "d"=> "def" })
+arr = [["john", 21], ["carl", 33], ["bob", 31]]
---
+++ first_spec.rb
@@ -0,0 +1,14 @@
+# require "rspec"
+require "./first"
+
+describe "#hash_to_array" do
+  let(:hash) { {"a"=> "abc", "b"=> "bcd", "d"=> "def"} }
+
+  it "should return an array" do
+    expect(hash_to_array(hash)).to be_a(Array)
+  end
+
+  it "should build an array on key values" do
+    expect(hash_to_array(hash)).to eq([["a", "abc"], ["b", "bcd"], ["d", "def"]])
+  end
+end
